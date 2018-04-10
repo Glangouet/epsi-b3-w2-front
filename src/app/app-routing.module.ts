64596546cde4ed5b268996bfefad74bd26274fdf
glangouet <guillaume@dialo-home.com>
2018-04-10 14:30:38 +0200

    add left menu component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {CreateProjectComponent} from './project/create-project/create-project.component';
 
 const appRoutes: Routes = [
-        { path: '', component: HomeComponent },
+        {
+            path: '', component: HomeComponent,
+            path: 'project/create', component: CreateProjectComponent
+        },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import {HomeComponent} from './home/home.component';
 import { ContentComponent } from './layout/content/content.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import {ProjectService} from './services/project.service';
+import {UserService} from './services/user.service';
+import {AuthService} from './services/auth.service';
+import { ProjectComponent } from './project/project.component';
+import { UserComponent } from './user/user.component';
+import { LeftMenuComponent } from './layout/left-menu/left-menu.component';
+import { CreateProjectComponent } from './project/create-project/create-project.component';
 
 
 @NgModule({
@@ -14,13 +21,21 @@ import { FooterComponent } from './layout/footer/footer.component';
     ContentComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProjectComponent,
+    UserComponent,
+    LeftMenuComponent,
+    CreateProjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+      AuthService,
+      UserService,
+      ProjectService
+  ],
   bootstrap: [AppComponent]
 })
 
