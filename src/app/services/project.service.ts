340bf3d077387b4975ad59d6fc18f5692f76dd00
glangouet <guillaume@dialo-home.com>
2018-04-10 11:04:03 +0200

    update services

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProjectService {
+
+  constructor() { }
+
+  addProject() {
+    // TODO
+  }
+
+  deleteProject() {
+    // TODO
+  }
+
+  updateProject() {
+    // TODO
+  }
+
+  getAllProjects() {
+    // TODO
+  }
+
+  getProjectById() {
+    // TODO
+  }
+
+  getProjectsByUser() {
+    // TODO
+  }
+
+  getUsersByProject() {
+    // TODO
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserService {
+
+  constructor() { }
+
+  addUser() {
+    // TODO
+  }
+
+  deleteUser() {
+    // TODO
+  }
+
+  updateUser() {
+    // TODO
+  }
+
+  getUser() {
+    // TODO
+  }
+
+  getAllUser() {
+    // TODO
+  }
+
+  getAllUserBySkills() {
+    // TODO
+  }
+
+  getAllUsersByCampusAndRoles() {
+    // TODO
+  }
+}
