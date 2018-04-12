70379a7c85b50d1834681f0d780a0210963cab13
glangouet <guillaume@dialo-home.com>
2018-04-12 18:30:25 +0200

    Merge conflict.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,71 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+
+import { ProjectMyComponent } from './project/project-my/project-my.component';
 import {ProjectComponent} from './project/project.component';
 import {ProjectViewComponent} from './project/project-view/project-view.component';
 import {ProjectCreateComponent} from './project/project-create/project-create.component';
-import {ProjectUpdateComponent} from './project/project-update/project-update.component';
+import { ProjectUpdateComponent } from './project/project-update/project-update.component';
+//import { ProjectDeleteComponent } from './project/project-delete/project-delete.component';
+
 import {UserComponent} from './user/user.component';
-import {UserViewComponent} from './user/user-view/user-view.component';
-import {AdminComponent} from './admin/admin.component';
+import { UserViewComponent } from './user/user-view/user-view.component';
+import { UserListComponent } from './admin/user-list/user-list.component';
 import {UserCreateComponent} from './admin/user-create/user-create.component';
-import {UserUpdateComponent} from './admin/user-update/user-update.component';
-import {ProjectMyComponent} from './project/project-my/project-my.component';
-import {UserListComponent} from './admin/user-list/user-list.component';
-import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+import { UserUpdateComponent } from './admin/user-update/user-update.component';
+import { UserDeleteComponent } from './admin/user-delete/user-delete.component';
+
 import { CampusCreateComponent } from './campus/campus-create/campus-create.component';
 import { CampusUpdateComponent } from './campus/campus-update/campus-update.component';
+import { CampusDeleteComponent } from './campus/campus-delete/campus-delete.component';
+
+import {AdminComponent} from './admin/admin.component';
+
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
 import { SkillCreateComponent } from './skill/skill-create/skill-create.component';
 import { SkillUpdateComponent } from './skill/skill-update/skill-update.component';
+import { SkillDeleteComponent } from './skill/skill-delete/skill-delete.component';
+
 import { RoleCreateComponent } from './role/role-create/role-create.component';
 import { RoleUpdateComponent } from './role/role-update/role-update.component';
+import { RoleDeleteComponent } from './role/role-delete/role-delete.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+
     { path: 'projects', component: ProjectComponent },
+    { path: 'projects/view/:id', component: ProjectViewComponent},
     { path: 'my-projects', component: ProjectMyComponent },
     { path: 'projects/create', component: ProjectCreateComponent },
-    { path: 'projects/update/:id', component: ProjectUpdateComponent },
-    { path: 'projects/view/:id', component: ProjectViewComponent},
-    { path: 'users', component: UserComponent},
-    { path: 'users/:id', component: UserViewComponent},
-    { path: 'admin', component: AdminComponent },
-    { path: 'admin/users', component: UserListComponent },
-    { path: 'admin/users/create', component: UserCreateComponent},
+  { path: 'projects/update/:id', component: ProjectUpdateComponent },
+  //{ path: 'projects/delete/:id', component: ProjectDeleteComponent },
+  
+  { path: 'admin', component: AdminComponent },
     { path: 'admin/users/view/:id', component: UserUpdateComponent },
+
+    { path: 'users', component: UserComponent},
+    { path: 'users/:id', component: UserViewComponent },
+    { path: 'admin/users', component: UserListComponent },
+    { path: 'admin/users/create', component: UserCreateComponent },
+    { path: 'admin/users/update/:id', component: UserUpdateComponent },
+    
+  //{ path: 'campus', component: CampusComponent },
     { path: 'campus/create', component: CampusCreateComponent },
-    { path: 'campus/update', component: CampusUpdateComponent },
+  { path: 'campus/update', component: CampusUpdateComponent },
+ // { path: 'campus/delete', component: CampusDeleteComponent },
+
     { path: 'skill/create', component: SkillCreateComponent },
-    { path: 'skill/update', component: SkillUpdateComponent },
+  { path: 'skill/update', component: SkillUpdateComponent },
+  //{ path: 'skill/delete', component: SkillDeleteComponent },
+  
     { path: 'role/create', component: RoleCreateComponent },
-    { path: 'role/update', component: RoleUpdateComponent },
+  { path: 'role/update', component: RoleUpdateComponent },
+  //{ path: 'role/delete', component: RoleDeleteComponent }
+
 ];
 
 @NgModule({
